a44c1fb970b533dccdf9771fdc850b1c2a74873c
Paul Henrich <paul@henrich.me>
2014-12-20 00:53:30 -0500

    not quite yet (distance func?)

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -9,3 +9,24 @@
 
 (defn doublets [word1 word2]
   "make me work")
+
+(defn distance [word1 word2]
+  (count
+   (filter
+    identity
+    (for [letter1 word1
+          letter2 word2]
+      (= letter1 letter2)))))
+
+(defn neighbors [word1 word2]
+  (filter
+   (fn [candidate]
+     (= 1 (distance word1 candidate)))
+   words))
+
+(neighbors "heal" "feel")
+
+(def letter1 [:a :b :c])
+(def letter2 [:a :b :d])
+
+
