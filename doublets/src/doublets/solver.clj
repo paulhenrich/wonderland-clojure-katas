e1d024889680254e3f438f9b87df0ff517105227
Paul Henrich <paul@henrich.me>
2014-12-20 09:51:09 -0500

    candidate-neighbors

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -20,12 +20,20 @@
 
 
 (defn neighbors [word1]
-  "A neighbor is a word with an edit distance of one"
+  "A neighbor is a word with a distance of one"
   (filter
    (fn [candidate]
      (= 1 (distance word1 candidate)))
    (words-of-size (count word1))))
 
+(defn candidate-neighbors [word1 word2]
+  "Neighbors that share a letter with the last word"
+  (filter
+   (fn [candidate]
+     (< (distance word1 candidate) (distance word2 candidate)))
+   (neighbors word1)))
+
+(candidate-neighbors "head" "tell")
 
 (neighbors "heal")
 
