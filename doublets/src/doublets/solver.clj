0c25d9ad0d61e48586fd2c05719062107a510f01
Paul Henrich <paul@henrich.me>
2014-12-22 12:28:22 -0500

    clean up workspace

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -29,19 +29,6 @@
       (not= word1 candidate)))
      (words-of-size (count word1))))
 
-#_(defn ^:dynamic doublets-seq [& doublet]
-  (let [head  (take ((comp dec count) doublet) doublet)
-        word1 (last head)
-        word2 (last doublet)]
-    (cond
-     (some #{word2} (neighbors word1))
-     "yay"
-     :else
-     (flatten (cons head
-              (list ((first (sufficient-neighbors word1 word2))) word2))))))
-
-#_(clojure.tools.trace/dotrace [doublets-seq
-                              sufficient-neighbors] (doublets-seq "head" "tail"))
 (defn doublets [& doublet]
   (let [head  (take ((comp dec count) doublet) doublet)
         word1 (last head)
@@ -49,7 +36,7 @@
   (cond
    (= (distance word1 word2) 1)
    doublet
-   (= 0 (count (sufficient-neighbor word1 word2)))
+   (= 0 (count (sufficient-neighbors word1 word2)))
    '()
    :else
     (apply doublets (flatten (cons head
@@ -57,13 +44,5 @@
 
 (doublets "head" "tall")
 (doublets "tall" "tail")
-(sufficient-neighbors "tell" "tail")
 (doublets "door" "lock")
-(some #{"foo"} ["foo" "baz"])
-(sufficient-neighbor "aaaaaa" "bbbbbbb")
-(take ((comp dec count) [:a :b :c]) [:a :b :c])
 
-(doublets "head" "heal")
-(def test-words ["head" "teal"])
-(cons (take 1 test-words)
-      (cons (sufficient-neighbor (first test-words) (second test-words)) (rest test-words)))
